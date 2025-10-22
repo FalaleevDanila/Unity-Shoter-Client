cabc2f83ec3a93cee9dd1ab2cf2036f2a4f5a9b7
FalaleevDanila <falaleevdanila@gmail.com>
2025-10-22 19:46:47 +0300

    add physics

--- Client/Assets/Scripts/Multiplayer/generated/Player.cs
+++ Client/Assets/Scripts/Multiplayer/generated/Player.cs
@@ -2,23 +2,28 @@
 // THIS FILE HAS BEEN GENERATED AUTOMATICALLY
 // DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
 // 
-// GENERATED USING @colyseus/schema 3.0.64
+// GENERATED USING @colyseus/schema 1.0.46
 // 
 
 using Colyseus.Schema;
-#if UNITY_5_3_OR_NEWER
-using UnityEngine.Scripting;
-#endif
 
 public partial class Player : Schema {
-#if UNITY_5_3_OR_NEWER
-[Preserve]
-#endif
-public Player() { }
 	[Type(0, "number")]
-	public float x = default(float);
+	public float pX = default(float);
 
 	[Type(1, "number")]
-	public float y = default(float);
+	public float pY = default(float);
+
+	[Type(2, "number")]
+	public float pZ = default(float);
+
+	[Type(3, "number")]
+	public float vX = default(float);
+
+	[Type(4, "number")]
+	public float vY = default(float);
+
+	[Type(5, "number")]
+	public float vZ = default(float);
 }
 
--- Client/Assets/Scripts/Multiplayer/generated/State.cs
+++ Client/Assets/Scripts/Multiplayer/generated/State.cs
@@ -2,20 +2,13 @@
 // THIS FILE HAS BEEN GENERATED AUTOMATICALLY
 // DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
 // 
-// GENERATED USING @colyseus/schema 3.0.64
+// GENERATED USING @colyseus/schema 1.0.46
 // 
 
 using Colyseus.Schema;
-#if UNITY_5_3_OR_NEWER
-using UnityEngine.Scripting;
-#endif
 
 public partial class State : Schema {
-#if UNITY_5_3_OR_NEWER
-[Preserve]
-#endif
-public State() { }
 	[Type(0, "map", typeof(MapSchema<Player>))]
-	public MapSchema<Player> players = null;
+	public MapSchema<Player> players = new MapSchema<Player>();
 }
 
